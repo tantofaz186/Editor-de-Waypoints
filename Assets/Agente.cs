76d51dd70993c9575887e192ab7676482ea78569
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2022-09-23 12:10:17 -0300

    Aad agente State

--- Assets/Agente.cs
+++ Assets/Agente.cs
@@ -1,27 +1,65 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum AgentState
+{
+    Start,
+    Moving,
+    Finished
+}
 
 public class Agente : MonoBehaviour
 {
     List<Node> path = new List<Node>();
+    AgentState state = AgentState.Start;
 
-    private void OnEnable()
-    {
-        transform.position = NodeManager.Instance.Inicial.transform.position;
-        path = PathFinding.Instance.FindPath();
-    }
 
     private void FixedUpdate()
+    {
+        switch (state)
+        {
+            case AgentState.Start:
+                WaitForPath();
+                break;
+            case AgentState.Moving:
+                Move();
+                break;
+            case AgentState.Finished:
+                ResetPath();
+                break;
+        }
+    }
+
+    private void ResetPath()
+    {
+        NodeManager.Instance.ResetPath();
+        state = AgentState.Start;
+    }
+
+    private void WaitForPath()
+    {
+        transform.position = NodeManager.Instance.Inicial.transform.position;
+    }
+    public void GetPath()
+    {
+        path = PathFinding.Instance.FindPath();
+        state = AgentState.Moving;
+    }
+    private void Move()
     {
         if (path.Count > 0)
         {
             transform.position = Vector3.MoveTowards(transform.position, path[0].transform.position, 0.1f);
-            if (transform.position == path[0].transform.position)
+            if (Vector3.Distance(transform.position, path[0].transform.position) < 0.1f)
             {
                 path.RemoveAt(0);
             }
+        }
+        else
+        {
+            state = AgentState.Finished;
         }
     }
 }
