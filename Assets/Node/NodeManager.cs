7591b9b09948c24a47af38e99d26654d13cd12b1
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2022-09-24 16:47:15 -0300

    Update NodeManager.cs

--- Assets/Node/NodeManager.cs
+++ Assets/Node/NodeManager.cs
@@ -9,22 +9,25 @@ public class NodeManager : Singleton<NodeManager>
     private Node inicial;
     public Node Inicial => inicial;
     private Node final;
-    public Node Final  => final;
+    public Node Final => final;
     private Node atual;
     private Node Selected;
     public List<Node> Nodes;
     [SerializeField] private GameObject nodePrefab;
     private Camera mainCamera;
     [SerializeField] private Agente agente;
-    Vector3 mousePos => new Vector3(Input.mousePosition.x, Input.mousePosition.y, 11 );
+    Vector3 mousePos => new Vector3(Input.mousePosition.x, Input.mousePosition.y, 11 + 65);
+
     private void Start()
     {
         Nodes = new List<Node>();
         inicial = Instantiate(nodePrefab).GetComponent<Node>();
+        inicial.transform.position = new Vector3(inicial.transform.position.x, -65, inicial.transform.position.z);
         Nodes.Add(inicial);
         atual = inicial;
         mainCamera = Camera.main;
     }
+
     public void MakeNewVertex(Vector3 position)
     {
         var newNode = Instantiate(nodePrefab, position, Quaternion.identity).GetComponent<Node>();
@@ -33,38 +36,33 @@ public class NodeManager : Singleton<NodeManager>
         {
             atual.AddNeighbor(newNode);
         }
+
         SetAtual(newNode);
     }
+
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            if(final != null) final.GetComponent<MeshRenderer>().material.color = Color.green;
+            Debug.Log("MouseButton Pressed");
+            if (final != null) final.GetComponent<MeshRenderer>().material.color = Color.green;
             Ray ray = mainCamera.ScreenPointToRay(mousePos);
-            if(Physics.Raycast(ray, out RaycastHit hitData, 30))
+            if (Physics.Raycast(ray, out RaycastHit hitData, 200) &&
+                hitData.collider.gameObject.GetComponent<Node>() != null)
             {
-                if(atual != null)
+                var node = hitData.collider.gameObject.GetComponent<Node>();
+                if (atual != null && node != atual)
                 {
-                    if(hitData.collider.gameObject.GetComponent<Node>() != null)
-                    {
-                        var node = hitData.collider.gameObject.GetComponent<Node>();
-                        if(node != atual)
-                        {
-                            atual.AddNeighbor(node);
-                            SetAtual(node);
-                        }
-                    }
+                    atual.AddNeighbor(node);
                 }
-                else if(hitData.collider.gameObject.GetComponent<Node>() != null)
-                {
-                    SetAtual(hitData.collider.gameObject.GetComponent<Node>());
-                }
+                SetAtual(node);
             }
             else
             {
                 MakeNewVertex(mainCamera.ScreenToWorldPoint(mousePos));
             }
         }
+
         if (Input.GetMouseButton(0))
         {
             MoveNode(atual);
@@ -73,49 +71,55 @@ public class NodeManager : Singleton<NodeManager>
         {
             SetAtual(null);
             Ray ray = mainCamera.ScreenPointToRay(mousePos);
-            if (Physics.Raycast(ray, out RaycastHit hitData, 30) &&
+            if (Physics.Raycast(ray, out RaycastHit hitData, 200) &&
                 hitData.collider.gameObject.GetComponent<Node>() != null)
             {
                 if (final == hitData.collider.gameObject.GetComponent<Node>())
                 {
                     agente.GetPath();
                 }
+
                 SetFinal(hitData.collider.gameObject.GetComponent<Node>());
             }
-            
         }
+
         inicial.GetComponent<MeshRenderer>().material.color = Color.cyan;
     }
+
     private void SetAtual(Node node)
     {
         if (atual != null) atual.GetComponent<MeshRenderer>().material.color = Color.white;
         atual = node;
         if (atual != null) atual.GetComponent<MeshRenderer>().material.color = Color.red;
-        
     }
+
     private void SetFinal(Node node)
     {
-        if(final != null) final.GetComponent<MeshRenderer>().material.color = Color.white;
+        if (final != null) final.GetComponent<MeshRenderer>().material.color = Color.white;
         final = node;
-        if(final != null) final.GetComponent<MeshRenderer>().material.color = Color.green;
+        if (final != null) final.GetComponent<MeshRenderer>().material.color = Color.green;
     }
+
     void MoveNode(Node node)
     {
         node.MoveNode(mainCamera.ScreenToWorldPoint(mousePos));
     }
+
     public void ResetPath()
     {
         foreach (var node in Nodes)
         {
             node.Reset();
         }
+
         SetInicial(final);
         SetAtual(inicial);
         SetFinal(null);
     }
+
     void SetInicial(Node node)
     {
-        if(inicial != null) inicial.GetComponent<MeshRenderer>().material.color = Color.white;
+        if (inicial != null) inicial.GetComponent<MeshRenderer>().material.color = Color.white;
         inicial = node;
     }
 }
